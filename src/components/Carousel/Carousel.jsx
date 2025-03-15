313217bcd1d7a21b2a713ef4f16324e2888771dc
Rakhsrb <suhrobrahmatullayev973132@gmail.com>
2025-03-15 17:16:41 +0500

    salom

--- src/components/Carousel/Carousel.jsx
+++ src/components/Carousel/Carousel.jsx
@@ -1,12 +1,15 @@
 import React, { useEffect, useRef, useState } from "react";
 
 import "./Carousel.css";
-import img1 from "../../assets/about-section/IMG_4916.webp";
-import img2 from "../../assets/about-section/IMG_4917.webp";
-import img6 from "../../assets/about-section/IMG_4922.webp";
-import img7 from "../../assets/about-section/IMG_4923.webp";
-import img8 from "../../assets/about-section/IMG_4925.webp";
-import img9 from "../../assets/about-section/photo_2025-02-12_13-29-55.webp";
+import img1 from "../../assets/photos/1.webp";
+import img2 from "../../assets/photos/2.webp";
+import img3 from "../../assets/photos/3.webp";
+import img4 from "../../assets/photos/4.webp";
+import img5 from "../../assets/photos/5.jpg";
+import img6 from "../../assets/photos/6.jpg";
+import img7 from "../../assets/photos/7.jpg";
+import img8 from "../../assets/photos/8.jpg";
+import img9 from "../../assets/photos/9.jpg";
 
 export const Carousel = () => {
   let radius = 300;
@@ -22,10 +25,10 @@ export const Carousel = () => {
   const [importedImages, _] = useState([
     img1,
     img2,
+    img3,
+    img4,
+    img5,
     img6,
-    img7,
-    img8,
-    img9,
     img7,
     img8,
     img9,
--- src/components/Cube-Carousel/CubeCarousel.jsx
+++ src/components/Cube-Carousel/CubeCarousel.jsx
@@ -4,12 +4,15 @@ import "swiper/css";
 import "swiper/css/effect-cube";
 import "swiper/css/pagination";
 import { EffectCube, Pagination, Autoplay } from "swiper/modules";
-import img1 from "../../assets/about-section/IMG_4916.webp";
-import img2 from "../../assets/about-section/IMG_4917.webp";
-import img6 from "../../assets/about-section/IMG_4922.webp";
-import img7 from "../../assets/about-section/IMG_4923.webp";
-import img8 from "../../assets/about-section/IMG_4925.webp";
-import img9 from "../../assets/about-section/photo_2025-02-12_13-29-55.webp";
+import img1 from "../../assets/photos/1.webp";
+import img2 from "../../assets/photos/2.webp";
+import img3 from "../../assets/photos/3.webp";
+import img4 from "../../assets/photos/4.webp";
+import img5 from "../../assets/photos/5.jpg";
+import img6 from "../../assets/photos/6.jpg";
+import img7 from "../../assets/photos/7.jpg";
+import img8 from "../../assets/photos/8.jpg";
+import img9 from "../../assets/photos/9.jpg";
 
 export default function CubeCarousel() {
   return (
@@ -28,11 +31,13 @@ export default function CubeCarousel() {
         modules={[EffectCube, Pagination, Autoplay]}
         className="h-full"
       >
-        {[img1, img2, img6, img7, img8, img9].map((img, index) => (
-          <SwiperSlide key={index} className="w-full h-[100%]">
-            <img src={img} className="w-full h-full object-cover" />
-          </SwiperSlide>
-        ))}
+        {[img1, img2, img3, img4, img5, img6, img7, img8, img9].map(
+          (img, index) => (
+            <SwiperSlide key={index} className="w-full h-[100%]">
+              <img src={img} className="w-full h-full object-cover" />
+            </SwiperSlide>
+          )
+        )}
       </Swiper>
     </div>
   );
